55cee5fe7faf6de3dff0f2a1c6b9e9def7d6a2bf
nabilaakhaiir <nabilaakhairunnisa07@gmail.com>
2025-09-26 22:41:00 +0700

    TUGAS PEKAN 3 PRAKALPRO KELAS A

---
+++ A_prakalpro_2025_2511531003/src/pekan3/OperatorBangunRuang.java
@@ -0,0 +1,30 @@
+package pekan3;
+import java.util.Scanner;
+public class OperatorBangunRuang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        Scanner input = new Scanner(System.in);
+
+		        // Input
+		        System.out.print("Masukkan alas segitiga: ");
+		        double a = input.nextDouble();
+
+		        System.out.print("Masukkan tinggi segitiga: ");
+		        double ts = input.nextDouble();
+
+		        System.out.print("Masukkan tinggi prisma: ");
+		        double t = input.nextDouble();
+
+		        // Hitung luas alas
+		        double L_alas = 0.5 * a * ts;
+
+		        // Hitung volume
+		        double V = L_alas * t;
+
+		        // Output
+		        System.out.println("Volume prisma segitiga = " + V);
+		
+	}
+
+}
