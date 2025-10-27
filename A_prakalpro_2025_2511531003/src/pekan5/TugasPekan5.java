1c765b18dd358512ec50cb94c12167efb8e918e0
nabilaakhaiir <nabilaakhairunnisa07@gmail.com>
2025-10-27 14:41:29 +0700

    Pekan5

---
+++ A_prakalpro_2025_2511531003/src/pekan5/TugasPekan5.java
@@ -0,0 +1,20 @@
+package pekan5;
+
+public class TugasPekan5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for (int i = 1; i <= 5; i ++) {
+				for (int j = 1; j <= 5-i; j++) {
+			    	   System.out.print(".");
+			}
+				 System.out.print (i);
+				 System.out.println();
+			
+		}
+	 }
+}
+		
+	
+
+
