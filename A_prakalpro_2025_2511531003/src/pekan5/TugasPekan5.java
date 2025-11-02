d5f086921248f020b1b19a4136429450cfdf1a35
nabilaakhaiir <nabilaakhairunnisa07@gmail.com>
2025-11-02 21:59:52 +0700

    tugas pekan 5

--- A_prakalpro_2025_2511531003/src/pekan5/TugasPekan5.java
+++ A_prakalpro_2025_2511531003/src/pekan5/TugasPekan5.java
@@ -4,122 +4,50 @@ public class TugasPekan5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("#");
-		for (int i = 0; i < 16; i ++) {
-			 System.out.print("=");			    	   
+			System.out.print("#");
+			for (int i = 0; i < 16; i ++) {
+				 System.out.print("=");			    	   
+				}
+			     System.out.print("#");
+			     System.out.println();
+			             
+			     for (int j = 0; j < 4; j ++) {
+			     System.out.print("|");
+			      for (int k = 0; k < (-2*j+6); k ++) {
+				    System.out.print(" ");		
+			      }
+				  System.out.print("<>");
+				    for (int l = 0; l < 4*j; l++) {
+			        System.out.print(".");	
+				  }	
+				    System.out.print("<>");
+				  
+				    for (int k = 0; k < (-2*j+6); k ++) {
+					    System.out.print(" ");	
+		}
+				    System.out.print("|");
+				    System.out.println();
+		}
+			     for (int j = 0; j < 4; j ++) {
+				     System.out.print("|");
+				      for (int k = 0; k < (2*j); k ++) {
+					    System.out.print(" ");		
+				      }
+					  System.out.print("<>");
+					    for (int l = 0; l < (-4*j+12); l++) {
+				        System.out.print(".");	
+					  }	
+					    System.out.print("<>");
+					  
+					    for (int k = 0; k < (2*j); k ++) {
+						    System.out.print(" ");	
 			}
-		     System.out.println("#");
-
-	   System.out.print("|");
-	      for (int line = 0; line < 6; line ++) {
-		    System.out.print(" ");		
-	      }
-		  System.out.print("<><>");
-		    for (int line = 0; line < 6; line ++) {
-	        System.out.print(" ");	
-		  }	
-		    System.out.println("|");
-			  
-	   System.out.print("|");
-		  for (int line = 0; line < 4; line ++) {
-		    System.out.print(" ");		
-	      }
-		    System.out.print("<>");
-			for (int i = 1; i <= 4; i++) {
-			System.out.print(".");	
-		  }
-			System.out.print("<>");
-			for (int line = 0; line < 4; line ++) {
-		    System.out.print(" ");	
-	      }  
-			System.out.println("|");
-			    	  
-		System.out.print("|");
-		  for (int line = 0; line < 2; line ++) {
-			System.out.print(" ");		
-		  }
-			System.out.print("<>");
-			for (int i = 1; i <= 8; i++) {
-			System.out.print(".");	
-		  }
-			System.out.print("<>");
-			for (int line = 0; line < 2; line ++) {
-			System.out.print(" ");	
-		  }
-			System.out.println("|");
-					    	  
-		System.out.print("|");
-		  for (int line = 0; line < 0; line ++) {
-			System.out.print(" ");		
-		  }	    	
-			System.out.print("<>");
-			for (int i = 1; i <= 12; i++) {
-			System.out.print(".");	
-		  }
-			System.out.print("<>");
-			for (int line = 0; line <0; line ++) {
-			System.out.print(" ");	
-		  }
-			System.out.println("|");
-			
-			System.out.print("|");
-			  for (int line = 0; line < 0; line ++) {
-				System.out.print(" ");		
-			  }	    	
-				System.out.print("<>");
-				for (int i = 1; i <= 12; i++) {
-				System.out.print(".");	
-			  }
-				System.out.print("<>");
-				for (int line = 0; line <0; line ++) {
-				System.out.print(" ");	
-			  }
-				System.out.println("|");
-							    	  
-		System.out.print("|");
-		  for (int line = 0; line < 2; line ++) {
-			 System.out.print(" ");		
-						 }
-			 System.out.print("<>");
-			 for (int i = 1; i <= 8; i++) {
-			 System.out.print(".");	
-		   }
-			 System.out.print("<>");
-			 for (int line = 0; line < 2; line ++) {
-			 System.out.print(" ");	
-			}
-			 System.out.println("|");
-									    	  
-		System.out.print("|");
-			for (int line = 0; line < 4; line ++) {
-			 System.out.print(" ");		
-			}
-			 System.out.print("<>");
-			 for (int i = 1; i <= 4; i++) {
-			 System.out.print(".");	
-			}
-			 System.out.print("<>");
-			 for (int line = 0; line < 4; line ++) {
-			 System.out.print(" ");	
-			}
-			 System.out.println("|");
-							 
-		 System.out.print("|");
-		   for (int line = 0; line < 6; line ++) {
-			 System.out.print(" ");		
-			}
-			 System.out.print("<><>");
-			 for (int line = 0; line < 6; line ++) {
-			 System.out.print(" ");	
-			}	
-			 System.out.println("|");
-							 
-		 System.out.print("#");
-		   for (int i = 0; i < 16; i ++) {
-		     System.out.print("=");			    	   
-		    }
-			 System.out.println("#");			  			  									  			  
-	  }
-	}
-
-
+					    System.out.println("|");
+		}
+			     System.out.print("#");
+					for (int i = 0; i < 16; i ++) {
+						 System.out.print("=");			    	   
+						}
+					     System.out.println("#");
+			     }
+		}
