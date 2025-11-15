6f5b7c1e3ffc694cb5067cb7e1423f42a46e3b78
nabilaakhaiir <nabilaakhairunnisa07@gmail.com>
2025-11-15 22:10:34 +0700

    tugas Alpro pekan 7_2511531003

---
+++ A_prakalpro_2025_2511531003/src/pekan7_2511531003/Akun_2511531003.java
@@ -0,0 +1,47 @@
+package pekan7_2511531003;
+
+public class Akun_2511531003 {
+	//variabel global
+	private String Username;
+	private String Password;
+	private String Email;
+	private int PIN, Pin;
+
+	//membuat mutator (setter)
+	public void setUsername (String Username) {
+		this.Username = Username;
+	}
+    public void setPassword (String password) {
+		this.Password = password;
+	}
+    public void setEmail (String email) {
+		this.Email = email;
+	}
+    public void setPIN (int Pin) {
+		this.PIN = Pin;
+    }
+    
+   //membuat accessor (getter)
+   public String getUsername() {
+     return Username;
+   }
+   public String getPassword() {
+     return Password;
+   }
+   public String getEmail() {
+	     return Email;
+   }
+   public int getPin() {
+	     return PIN;
+   }
+   
+   public boolean isPasswordValid() {
+       return Password != null && Password.length() >=8;
+   }
+   public boolean isEmailValid() {
+	   return Email != null && Email.contains("@") && Email.contains(".");
+   
+   }
+   
+   }
+//nabila khairunnisa_2511531003
---
+++ A_prakalpro_2025_2511531003/src/pekan7_2511531003/tugasAlproPekan7_2511531003.java
@@ -0,0 +1,53 @@
+package pekan7_2511531003;
+
+import java.util.Scanner;
+
+public class tugasAlproPekan7_2511531003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+		System.out.println ("===== REGISTRASI AKUN BARU =====");
+		
+		  System.out.print("Masukkan Username     : ");
+	        String i = input.nextLine();
+	        System.out.print("Masukkan Password     : ");
+	        String j = input.nextLine();
+	        System.out.print("Masukkan Email        : ");
+	        String k = input.nextLine();
+	        System.out.print("Masukkan PIN (6 digit): ");
+	        int l = input.nextInt();
+	        
+	        System.out.println();
+	        Akun_2511531003 a = new Akun_2511531003();
+	        a.setUsername(i);
+	        a.setPassword(j);
+	        a.setEmail(k);
+	        a.setPIN(l);
+	      
+	        if (a.isPasswordValid() && a.isEmailValid()) {
+	    		System.out.println("--- REGISTRASI BERHASIL ---");
+	    		System.out.println("Akun untuk \"" + i + "\" telah berhasil dibuat");
+	    		System.out.println();
+	        } else	{
+	        	System.out.println("--- REGISTRASI GAGAL ---");
+	        	System.out.println("Email Anda \"" + i + "\" tidak valid (harus mengandung '@' dan '.') ");
+	        	System.out.println();
+	}
+	    	System.out.println("--- DETAIL AKUN ---");
+	    	System.out.print("Username (Lowercase) : ");
+	    	System.out.println (i.toLowerCase ());
+	    	System.out.print("Email(Uppercase)     : ");
+	    	System.out.println (k.toUpperCase ());
+	    	System.out.print("ID Pengguna(Gabungan): ");
+	    	System.out.println (i + l);
+	
+	    	System.out.println();
+	        System.out.println("--- Uji Tipe Data (PIN Anda: "+l+") ---");
+	        int hasilInt = a.getPin() + 10;
+	        System.out.println ("PIN (int) + 10      = " + hasilInt);
+	        String hasilString = l + "10";
+	        System.out.println ("PIN (String) + \"" + 10 + "\" = " + hasilString);
+}
+	}
+//nabila khairunnisa_2511531003
